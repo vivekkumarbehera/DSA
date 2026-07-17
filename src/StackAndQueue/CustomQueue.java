c8eeab7cc0385228e35f87d80eaa35c84a19638f
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-07-17 11:33:49 +0530

    Queue

--- src/StackAndQueue/CustomQueue.java
+++ src/StackAndQueue/CustomQueue.java
@@ -1,2 +1,51 @@
-package StackAndQueue;public class CustomQueue {
+package StackAndQueue;
+
+public class CustomQueue {
+    private int[] data;
+    private static final int default_size=10;
+    int end = 0;
+    public CustomQueue(){
+        this(default_size);
+    }
+    public CustomQueue(int size) {
+        this.data = new int[size];
+    }
+    public boolean isfull(){
+      return   end == data.length;
+    }
+    public boolean isempty(){
+        return end ==  0;
+    }
+    public boolean insert(int item){
+         if(isfull()){
+             return false;
+         }
+         data[end++]=item;
+         return true;
+    }
+    public int remove() throws Exception{
+        if(isempty()){
+            throw new Exception("Queue is Empty !!");
+        }
+        int removed = data[0];
+        for (int i = 1; i < end ; i++) {
+            data[i-1]= data[i];
+        }
+        end --;
+        return removed;
+    }
+    public int font () throws Exception{
+        if(isempty()){
+            throw new Exception("Queue is empty");
+        }
+        return data[0];
+    }
+    public void display(){
+        for (int i = 0; i <end ; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("END");
+    }
+
+
 }

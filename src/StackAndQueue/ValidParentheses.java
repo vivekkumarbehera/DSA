b823f5b4c407074eedea8518e540bbb8d16f03f4
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-07-17 12:14:35 +0530

    ValidParenthesis

---
+++ src/StackAndQueue/ValidParentheses.java
@@ -0,0 +1,33 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) return false;
+                char top = st.pop();
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));      // true
+        System.out.println(isValid("()[]{}"));  // true
+        System.out.println(isValid("(]"));      // false
+        System.out.println(isValid("([)]"));    // false
+        System.out.println(isValid("{[]}"));    // true
+    }
+}

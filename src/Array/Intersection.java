bd3dbe6190754b125557fc7a4eb80a39867e41c1
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-07-25 17:47:44 +0530

    fixed

--- src/Array/Intersection.java
+++ src/Array/Intersection.java
@@ -1,2 +1,36 @@
-package Array;public class Intersection {
+package Array;
+
+import java.util.Arrays;
+
+public class Intersection {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,1};
+        int[] nums2={2,2};
+        System.out.println(Arrays.toString(intersection(nums, nums2)));
+
+    }
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+       int i =0;
+       int j =0;
+       int k =0;
+       int result[]=new int[nums1.length+ nums2.length];
+       while(i< nums1.length || j< nums2.length){
+           if(nums1[i]==nums2[j]){
+               if(k==0 || result[k-1] != nums1[i]){
+                   result[k++]=nums1[i];
+               }
+               i++;
+               j++;
+           } else if (nums1[i] < nums2[j]) {
+               i++;
+           }
+          else{
+              j++;
+           }
+       }
+
+       return Arrays.copyOfRange(result,0,k);
+    }
 }

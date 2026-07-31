2f20983bb51a599113bae4a23b69d649bf878b66
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-08-01 00:00:17 +0530

    BST

---
+++ src/BinarySearchTree/BST.java
@@ -0,0 +1,49 @@
+package BinarySearchTree;
+
+public class BST {
+    public class Node {
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public Node root;
+
+    public BST() {}
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isempty() {
+        return root == null;
+    }
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            return new Node(value);
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        } else if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+}

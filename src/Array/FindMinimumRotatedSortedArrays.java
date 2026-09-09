46379f183d4608d4865c7f803d24c4c3a7901f76
VivekKumarbehra <vivekbehera0807@gmail.com>
2026-09-09 12:36:06 +0530

    RotatedMinimumFindArray

---
+++ src/Array/FindMinimumRotatedSortedArrays.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class FindMinimumRotatedSortedArrays {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length-1;
+        int ans = Integer.MAX_VALUE;
+        while(low <= high){
+            int mid = (low + high) /2;
+            if (nums[low] <= nums[mid]){
+                ans = Math.min(ans,nums[low]);
+                low = mid +1;
+
+            }
+            else {
+                high=mid-1;
+                ans=Math.min(ans,nums[mid]);
+            }
+        }
+        return ans;
+    }
+}
